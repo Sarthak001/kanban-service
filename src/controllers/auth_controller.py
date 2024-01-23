80d267c7886aa542470f39cc42b51d5c9657bf7b
Nupur201198 <nupur.tomar20@gmail.com>
2024-01-23 23:56:22 +0530

    Updated register and email verification

--- src/controllers/auth_controller.py
+++ src/controllers/auth_controller.py
@@ -4,6 +4,7 @@ from src.models.user_model import User
 from src.models.user_role_model import UserRole
 from src.models.otp_model import Otp
 from src import bcrypt, db, mail
+from src.utils import otp_service
 from datetime import datetime
 import jwt
 import os
@@ -13,7 +14,6 @@ from random import randint
 auth = Blueprint("auth", __name__)
 
 # route for signup api/v1/auth/signup
-
 
 @auth.route('/signup', methods=["POST"])
 def handle_signup():
@@ -40,9 +40,17 @@ def handle_signup():
                 )
                 db.session.add(user_obj)
                 db.session.commit()
+                signed_up = user_obj
                 msg = Message("Kanban Board email verification code", sender="0176cs171103@gmail.com",
-                              recipients=["nupur.tomar20@gmail.com"])
-                msg.body = "Generate"
+                              recipients=[f"{user_obj.email}"])
+                otp_generated = otp_service.GenerateOtp()
+                otp_obj = Otp(
+                    user_id_fk = user_obj.user_id,
+                    otp = otp_generated
+                )
+                db.session.add(otp_obj)
+                db.session.commit()
+                msg.body = str(otp_generated)
                 mail.send(msg)
                 return Response(
                     json.dumps(
@@ -50,7 +58,7 @@ def handle_signup():
                             "status": "success",
                             "error": None,
                             "data": {
-                                "message": f"User created successfully. Please verfiy your email id. Verification link has been sent to Email:{user_obj.email}"
+                                "message": f"User created successfully. Please verfiy your email id. Verification code has been sent to Email:{user_obj.email}"
                             }
                         }),
                     status=201,
@@ -70,9 +78,25 @@ def handle_signup():
         )
 
 
+@auth.route('/verifyEmail',methods = ["GET"])
+def handle_verifyEmail():
+    email_res = request.args.get('email')
+    otp_res = int(request.args.get('otp'))
+    user_obj= User.query.filter_by(email = email_res).first()
+    if user_obj:
+        otp_db = db.session.query(Otp).order_by(Otp.id.desc()).first()
+        if otp_res == otp_db.otp:
+            user_obj.is_active = True
+            db.session.commit()
+            return "Sucessfully Verified"     
+        return "Wrong Otp"
+    else:
+        return "User does not exist. Please register again!!"
+
 # route for login api/users/signin
 @auth.route('/signin', methods=["POST"])
 def handle_login():
+
     return
 
 
