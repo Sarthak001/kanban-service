c226da2908c3a0396fc40f46b3e867cd99b5256f
Nupur201198 <nupur.tomar20@gmail.com>
2024-01-23 14:02:03 +0530

    Created basic sign up

--- src/controllers/auth_controller.py
+++ src/controllers/auth_controller.py
@@ -6,6 +6,7 @@ from src import bcrypt, db
 from datetime import datetime
 import jwt
 import os
+from random import randint
 
 # user controller blueprint to be registered with api blueprint
 auth = Blueprint("auth", __name__)
@@ -13,18 +14,43 @@ auth = Blueprint("auth", __name__)
 # route for signup api/users/signup
 
 
-@auth.route('/signup', methods=["GET"])
+@auth.route('/signup', methods=["POST"])
 def handle_signup():
-    user_obj = User.query.filter_by(user_id=1).first()
-    otp_obj = Otp(
-        user_id_fk = user_obj.user_id,
-        otp = 567834
-    )
-    db.session.add(otp_obj)
-    db.session.commit()
-    return "hello world"
+    data = request.json
+    if "first_name" in data and "last_name" in data and "email" in data and "password" in data:
+        user = User.query.filter_by(email=data["email"]).first()
+        if user:
+            return Response(response=json.dumps({'status': "failed", "message": "User already exists"}))
+        else:
+            role = UserRole.query.filter_by(role_name="normal user").first()
+            # print(role.role_id)
+            user_obj = User(
+                role_id_fk=role.role_id,
+                user_name=data['first_name'] +
+                data['last_name'] + str(randint(10, 99)),
+                first_name=data['first_name'],
+                last_name=data['last_name'],
+                email=data['email'],
+                passwd=bcrypt.generate_password_hash(
+                    str(data['password'])).decode('utf-8'),
+                is_active=True,
+                last_login=datetime.now()
+            )
+            db.session.add(user_obj)
+            db.session.commit()
 
+            payload = {
+                'firstname': user_obj.first_name,
+                'lastname': user_obj.last_name,
+                'username': user_obj.user_name,
+                'email': user_obj.email,
+            }
+            token = jwt.encode(payload=payload, key=os.getenv(
+                'SECRET_KEY'), algorithm='HS256')
 
+            return Response(response=json.dumps({"status": "Sucess", "Message": f"User successfully created for {data['first_name']} as {user_obj.user_name}", "token": token}))
+    else:
+        return Response(response=json.dumps({"status": "failed", "message": "Enter correct details"}))
 # route for login api/users/signin
 
 
